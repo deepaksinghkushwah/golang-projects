aeb1d840154bebeb79f39d903277d82583533916
Deepak Singh Kushwah <deepaksinghkushwah@gmail.com>
2019-06-10 08:29:20 +0530

    updates

--- app-youtube-download/main.go
+++ app-youtube-download/main.go
@@ -11,18 +11,22 @@ import (
 )
 
 func main() {
+	var youtubeURL string
+	var filename string
+	flag.StringVar(&youtubeURL, "youtubeURL", "youtube video url", "Url of video")
+	flag.StringVar(&filename, "filename", "dl.mp4", "filename for file")
 	flag.Parse()
 	log.Println(flag.Args())
 	usr, _ := user.Current()
 	currentDir := fmt.Sprintf("%v/Movies/youtubedr", usr.HomeDir)
 	log.Println("download to dir=", currentDir)
 	y := youtube.NewYoutube(true)
-	y.DecodeURL("https://www.youtube.com/watch?v=_TNVgALagok")
-	arg := flag.Arg(0)
-	if err := y.DecodeURL(arg); err != nil {
+	y.DecodeURL(youtubeURL)
+	//arg := flag.Arg(0)
+	if err := y.DecodeURL(youtubeURL); err != nil {
 		fmt.Println("err:", err)
 	}
-	if err := y.StartDownload(filepath.Join(currentDir, "dl.mp4")); err != nil {
+	if err := y.StartDownload(filepath.Join(currentDir, filename)); err != nil {
 		fmt.Println("err:", err)
 	}
 }
